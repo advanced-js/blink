bd9e3768a9e034b0ad6d855899881cd3e18466f4
Damian Kowal <damian@damiankowal.com>
2014-04-06 14:57:04 -0400

    Blink function working for the first case (speed = 1000)

--- blink.js
+++ blink.js
@@ -3,8 +3,20 @@
     $.fn.blink = function( speed ) {
 
         // Make the element blink
+        function toggleVisibility( element ) {
+            function isVisible( element ) {
+                return element.css( "visibility" ) === "visible";
+            }
+            return function () {
+                element.css({
+                    visibility: isVisible( element ) ? "hidden" : "visible"
+                });
+            };
+        }
 
-        return this; // Enable chaining
+        window.setInterval( toggleVisibility( this ), speed);
+
+        return this // Enable chaining
  
     };
  
