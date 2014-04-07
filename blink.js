e127444069a4c70b15b64be102727cd49f1f889f
Damian Kowal <damian@damiankowal.com>
2014-04-07 08:59:34 -0400

    Rename functions and add comment to make things clearer

--- blink.js
+++ blink.js
@@ -5,16 +5,20 @@
         // Set a default speed incase the argument passed is not valid
         speed = ( typeof speed === "number" && speed > 0 ) ? speed : 500;
 
-        function toggleVisibility( element ) {
-            function toggle( index, visibility ) {
+        function visibilityToggler( element ) {
+            // A function which takes as its argument a jQuery-wrapped element
+            // and returns a function which toggles that element's visibility
+
+            function toggleVisibility ( index, visibility ) {
                 return ( visibility === "visible" ) ? "hidden" : "visible";
             }
+
             return function () {
-                element.css( "visibility", toggle );
+                element.css( "visibility", toggleVisibility );
             };
         }
 
-        window.setInterval( toggleVisibility( this ), speed);
+        window.setInterval( visibilityToggler( this ), speed );
 
         return this; // Enable chaining
  
