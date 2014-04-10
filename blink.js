1fba52f606aeb4dff167fa858a42d50b15e65a39
Damian Kowal <damian@damiankowal.com>
2014-04-10 00:38:32 -0400

    This is a simpler way to write blink

--- blink.js
+++ blink.js
@@ -1,27 +1,19 @@
 ;(function( $, window, undefined ) {
- 
+
     $.fn.blink = function( speed ) {
+        var element = this;
 
         // Set a default speed incase the argument passed is not valid
         speed = ( typeof speed === "number" && speed > 0 ) ? speed : 500;
 
-        function visibilityToggler( element ) {
-            // A function which takes as its argument a jQuery-wrapped element
-            // and returns a function which toggles that element's visibility
-
-            function toggleVisibility( index, visibility ) {
-                return ( visibility === "visible" ) ? "hidden" : "visible";
-            }
-
-            return function() {
-                element.css( "visibility", toggleVisibility );
-            };
+        function toggleElementVisibility() {
+            element.css( "visibility", function( index, value ) {
+                return ( value === "visible" ) ? "hidden" : "visible";
+            });
         }
 
-        window.setInterval( visibilityToggler( this ), speed );
-
+        window.setInterval( toggleElementVisibility, speed );
+        
         return this; // Enable chaining
- 
     };
- 
 }( jQuery, window ));
