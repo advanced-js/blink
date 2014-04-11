611b808542d3cbd88453b2eec39fc298526bfc11
Ross Christie <rosspchristie@gmail.com>
2014-04-11 17:18:24 -0400

    blink exercise

--- blink.js
+++ blink.js
@@ -1,1 +1,20 @@
-// YOUR CODE GOES HERE
+(function($) {
+	$.fn.blink = function(time) {
+		var blinkItem = this, see = true;
+		function transparent(){
+			 $(blinkItem).css('opacity','0');
+			 see = false;
+		}
+		function opaque(){
+			 $(blinkItem).css('opacity','100');
+			 see = true;
+		}
+		setInterval(function() {
+			if (see === true) {
+				transparent();
+			} else if (see === false) {
+				opaque();
+			}
+		},time);
+	};
+}(jQuery));
