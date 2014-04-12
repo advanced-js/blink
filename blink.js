02c0559a6707e3b3147327055c63b3a85ff65fd2
msav364 <msav364@earthlink.net>
2014-04-11 22:20:54 -0400

    Blink exercise

--- blink.js
+++ blink.js
@@ -1,1 +1,10 @@
 // YOUR CODE GOES HERE
+
+(function($) {
+
+  $.fn.blink = function(interval) {
+   var thistarget = this; 
+   var blinkthis = setInterval(function() { thistarget.toggle(); },interval);
+  };
+
+}(jQuery));
