1004d70d9e4eda8d67663091ff35fa53e70234de
msquarednet <msquarednet@gmail.com>
2014-06-01 16:06:15 -0400

    create Blinky

--- blink.js
+++ blink.js
@@ -1,1 +1,13 @@
-// YOUR CODE GOES HERE
+(function ( $ ) {
+
+	$.fn.blink = function(seconds) {
+		var that = this;
+		var i=0;
+		var clearIntervalID = setInterval(function() {
+			that.css( "visibility", (++i%2===0) ? "visible" : "hidden" );
+		},seconds);
+		return this;
+	};
+	
+}( jQuery ));
+
