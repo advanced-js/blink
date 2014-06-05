3ca445afe4b4a9294e3cd58fba741960b651875b
mroyt <michael.roytburd@gmail.com>
2014-06-05 09:22:54 -0400

    first try

--- blink.js
+++ blink.js
@@ -1,1 +1,15 @@
 // YOUR CODE GOES HERE
+
+jQuery.fn.blink=function(speed){
+	var color = this.css("color");
+	setInterval(function(){
+		this.css("color","none")
+	},speed/2)
+	setInterval(function(){
+		this.css("color",color)
+	},(3*speed)/2)
+}
+
+jQuery('.myDiv').blink(1000);
+// twice as fast
+jQuery('.otherDiv').blink(500);
