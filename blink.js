1139b214f00a230cf2883f4b8d6c041d44f090da
mroyt <michael.roytburd@gmail.com>
2014-06-06 08:38:44 -0400

    mroyt blink completed - questions embedded
    questions in the comments:  /*Why do we need to use the "$" to make this
    a jQuery element?
    I'm having trouble understanding some of the logical flow
    of the $ function combined with "this."*/
    
    /*ALso, I originally tried using "this" with the fadeOut and fadeIn,
    but it didn't work until I pulled it out and put it into the el
    variable. Why is that?*/

--- blink.js
+++ blink.js
@@ -1,13 +1,24 @@
-// YOUR CODE GOES HERE
+//by mroyt
 
-jQuery.fn.blink=function(speed){
-	var color = this.css("color");
+$.fn.blink=function(speed){
+	/*Why do we need to use the "$" to make this a jQuery element? 
+	I'm having trouble understanding some of the logical flow 
+	of the $ function combined with "this."*/
+
+	/*ALso, I originally tried using "this" with the fadeOut and fadeIn, 
+	but it didn't work until I pulled it out and put it into the el
+	variable. WHy is that?*/
+	var el = $(this);
+	var isVisible = true;
 	setInterval(function(){
-		this.css("color","none")
-	},speed/2)
-	setInterval(function(){
-		this.css("color",color)
-	},(3*speed)/2)
+		if (isVisible){
+			el.fadeOut(0);
+		}
+		else{
+			el.fadeIn(0);
+		}
+		isVisible = !isVisible;
+	},speed);
 }
 
 jQuery('.myDiv').blink(1000);
