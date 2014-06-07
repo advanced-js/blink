50ba355d21440d10147a20a35878bba0541d30bc
Justine Tiu <justine.r.tiu@gmail.com>
2014-06-06 23:43:29 -0400

    Justine's blink exercise but I'm not sure why I had to use  instead of just this

--- blink.js
+++ blink.js
@@ -1,1 +1,14 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(msec) {
+	var obj = this;
+	setInterval(function() {
+		if ($(obj).css('display') == 'none') {
+			$(obj).css('display', 'block');
+		}
+		else {
+			$(obj).css('display', 'none');
+		}
+	}, msec);
+	return;
+};
+
+// Inspired by http://jsfiddle.net/Sk8erPeter/Yca4Z/
