0c4579e030acea8ae5ab871a8e6800c1ecd61d52
Joe Burdick <josephdburdick@gmail.com>
2014-06-07 08:58:15 -0400

    Blink exercise with variable speed.

--- blink.js
+++ blink.js
@@ -1,1 +1,20 @@
-// YOUR CODE GOES HERE
+(function($){
+	$.fn.blink = function(delay){
+		// var defaults = {
+		// 	delay: 500
+		// };
+		// options = $.extend(defaults, options);
+
+		return this.each(function(){
+			var obj = this;
+			setInterval(function(){
+				if ($(obj).css('visibility') == 'visible'){
+					$(obj).css('visibility', 'hidden');
+				}
+				else {
+					$(obj).css('visibility', 'visible');
+				}
+			}, delay);
+		});
+	};
+}(jQuery));
