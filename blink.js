901805dc758b1ff437966f573c3bea47b55b4c33
DJ Rosenbaum <dave@djrconcepts.com>
2014-09-26 02:01:10 -0400

    adding djrconcepts blink jquery plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,32 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(blink_interval) {
+
+    var blink_state = true;
+
+    var selected_element = this;
+
+    setInterval(function(){
+
+        //console.log('blink');
+
+        if ( blink_state === true ) {
+
+            //console.log('hide element');
+
+            selected_element.css('visibility', 'hidden');
+
+            blink_state = false;
+
+        }
+
+        else {
+
+            //console.log('show element');
+
+            selected_element.css('visibility', 'visible');
+
+            blink_state = true;
+        }
+
+    }, blink_interval);
+
+};
