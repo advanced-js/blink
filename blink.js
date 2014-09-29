1f8b9bdc2da65edd3fdec316fa640e421f32a295
tsjoberg <tomgsjoberg@gmail.com>
2014-09-28 20:33:42 -0400

    blink assignment
    works with all jquery objects

--- blink.js
+++ blink.js
@@ -1,1 +1,12 @@
-// YOUR CODE GOES HERE
+(function($) {
+
+    $.fn.blink = function(msec) {
+        var elems = this;
+        
+        setInterval(function(){
+          elems.toggle();
+        }, msec);
+
+        return this;
+    }
+}(jQuery));
