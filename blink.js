9b6386af9c0ce023f33695fd9850150efc19ca53
Kim <jean@jeanyoungkim.com>
2014-09-30 17:12:40 -0400

    Jean Kim - completed blink exercise

--- blink.js
+++ blink.js
@@ -1,1 +1,16 @@
 // YOUR CODE GOES HERE
+(function($) {
+
+    $.fn.blink = function(seconds) {
+    	var view = $(this);
+    	setTimeout(function() {
+	    	if(view.css('opacity') === '0') {
+	    		view.css('opacity', '1');
+	    	} else {
+	    		view.css('opacity', '0');
+	    	}
+    		view.blink(seconds);
+    	}, seconds);
+    };
+
+}(jQuery));
