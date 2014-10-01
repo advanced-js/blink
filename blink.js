516fbed8f4ff558fa542bca0ea8a840d45b6e77f
Suzie Grange <suzie.g@me.com>
2014-09-30 23:16:39 -0400

    one blinker...

--- blink.js
+++ blink.js
@@ -1,1 +1,20 @@
-// YOUR CODE GOES HERE
+(function ($) {
+  $.fn.blink = function (secs) {
+    var self = this;
+    //store default state as property of function object, if not already defined
+    if (self.blinkOn === undefined) {
+      self.blinkOn = false;
+    }
+    //console.log(self.blinkOn);
+    setTimeout(function () {
+      if (self.blinkOn === true) {
+        self.css({'visibility': 'visible'});
+        self.blinkOn = false;
+      } else {
+        self.css({'visibility': 'hidden'});
+        self.blinkOn = true;
+      }
+      self.blink(secs);
+    }, secs);
+  };
+}(jQuery));
