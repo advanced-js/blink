e0c70baf06313543b35ba207fe41a01061122462
alexoviedo <alexoviedo999@gmail.com>
2014-10-01 00:17:48 -0400

    This is V1

--- blink.js
+++ blink.js
@@ -1,1 +1,23 @@
-// YOUR CODE GOES HERE
+(function( $ ) {
+ 
+    $.fn.blink = function( pause ) {
+        that = this;      
+        setInterval(function(){
+          pause = pause + 1000;
+          if(pause % 2000 ===0){
+            $(that).css( "visibility", "none" );
+            // console.log('pause-1');
+          }
+          else{
+             $(that).css( "visibility", "hidden" );
+             // console.log('pause-2');
+          }
+          
+        }, pause);                 
+    return this;
+    };
+ 
+}( jQuery ));
+
+
+$( ".myDiv" ).blink(1000);
