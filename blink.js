7dbcc05a98adb7195c571601b5b3a52ce2659589
Suzie Grange <suzie.g@me.com>
2014-09-30 23:40:42 -0400

    sharing class name in html

--- blink.js
+++ blink.js
@@ -1,21 +1,19 @@
 (function ($) {
   $.fn.blink = function (secs) {
     var self = this;
-    return self.each(function () {
-      //store default state as property of function object, if not already defined
-      if (self.blinkOn === undefined) {
+    //store default state as property of function object, if not already defined
+    if (self.blinkOn === undefined) {
+      self.blinkOn = false;
+    }
+    setTimeout(function () {
+      if (self.blinkOn === true) {
+        self.css({'visibility': 'visible'});
         self.blinkOn = false;
+      } else {
+        self.css({'visibility': 'hidden'});
+        self.blinkOn = true;
       }
-      setTimeout(function () {
-        if (self.blinkOn === true) {
-          self.css({'visibility': 'visible'});
-          self.blinkOn = false;
-        } else {
-          self.css({'visibility': 'hidden'});
-          self.blinkOn = true;
-        }
-        self.blink(secs);
-      }, secs);
-    });
+      self.blink(secs);
+    }, secs);
   };
 }(jQuery));
