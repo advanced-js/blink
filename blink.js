18842bd21cd71c5da9d224e9e3bd4640517d346a
Emily O'Brien <emily.obrien@refinery29.com>
2014-09-30 21:40:34 -0400

    blink plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,22 @@
-// YOUR CODE GOES HERE
+(function($){
+
+	$.fn.blink = function(interval){
+	  visible = true;
+	  self = this
+
+        setInterval(function(){
+
+          if(visible === true){
+            self.css("display", "none");
+            visible = false;
+          }
+          else if(visible === false){
+            self.css("display", "block");
+            visible = true;
+          }
+
+        }, interval);
+
+	};
+
+}(jQuery));
