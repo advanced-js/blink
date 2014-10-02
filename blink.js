bdcbfde93ed83a924ab1ca739581cff62c2999b3
Jen <jennifer.calloway@refinery29.com>
2014-10-02 13:18:36 -0400

    added blink plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,22 @@
-// YOUR CODE GOES HERE
+
+  (function( $ ) {
+    
+    $.fn.blink = function(speed) {
+      var element = this;
+
+      setInterval(function() {
+        if (element.css('visibility') == 'visible') {
+          $(element).css('visibility', 'hidden');
+        } else {
+          visible();
+        }
+      }, speed);
+
+      var visible = function() {
+        $(element).css('visibility', 'visible');
+      }
+
+      return this;
+    };
+  
+  }( jQuery ));
