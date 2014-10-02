1049363b55666c899de072ff6c2f964a35f2c303
Jon Culver <jonathanhculver@gmail.com>
2014-10-01 22:31:50 -0400

    fixing jshint errors

--- blink.js
+++ blink.js
@@ -5,13 +5,13 @@
 		var flash = function(that) {
 			setInterval(function(){
 				var display = that.style.visibility;
-				if(display == 'visible' || display == '') {
+				if(display === 'visible' || display === '') {
 					that.style.visibility = 'hidden';
 				} else {
 					that.style.visibility = 'visible';
 				}
 			}, interval);
-		}
+		};
 
 		return this.each(function() {
 			flash(this);
