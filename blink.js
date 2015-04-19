87551c013df94d58a4e40068084406885da64376
Alek <agrinb@gmail.com>
2015-04-19 14:30:38 -0400

    Blink function works with setInterval

--- blink.js
+++ blink.js
@@ -1,1 +1,19 @@
-// YOUR CODE GOES HERE
+//Note to self. More info on plugin creation at https://learn.jquery.com/plugins/basic-plugin-creation/
+(function($)
+{
+  $.fn.blink = function(options) {
+    return this.each(function() {
+      var obj = $(this);
+      setInterval(function() {
+        if($(obj).css("visibility") == "visible")
+        {
+          $(obj).css('visibility','hidden');
+        }
+        else
+        {
+          $(obj).css('visibility','visible');
+        }
+      }, options);
+    });
+  }
+}(jQuery))
