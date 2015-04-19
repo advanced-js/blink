dffad9ed42b63a40f66f3903e5cb42ab0c537733
Emily Maskin <emily.maskin@gmail.com>
2015-04-19 17:05:58 -0400

    jquery plugin that makes elements blink

--- blink.js
+++ blink.js
@@ -1,1 +1,13 @@
-// YOUR CODE GOES HERE
+(function($) {
+  $.fn.blink = function(interval) {
+    var _this = this;
+    setInterval(function() {
+      // use css visibility instead of .toggle() so elements don't jump around
+      if (_this.css('visibility') === 'visible') {
+        _this.css('visibility', 'hidden');
+      } else {
+        _this.css('visibility', 'visible');
+      }
+    }, interval);
+  };
+}(jQuery));
