1d3fef64719c7762f61585ed0b9227e4222180f9
Caroline Steffen <csteffen@renttherunway.com>
2015-04-21 00:11:23 -0400

    creating blink jquery plugin

--- blink.js
+++ blink.js
@@ -1,1 +1,11 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(interval) {
+	var self = this;
+
+	setInterval( function() {
+		if (self.css("visibility") === "visible") {
+			self.css("visibility", "hidden");
+		} else {
+			self.css("visibility", "visible");
+		}
+	}, interval);
+};
