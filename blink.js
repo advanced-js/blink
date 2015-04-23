a4f72ab2e5d8a80e944ad995d1d039778f51ca68
AlexisFearon <alexisfearin@MBP0221.local>
2015-04-23 13:35:31 -0400

    alexis_blink
    I know this is not exactly right but wanted to at least give it this
    try.

--- blink.js
+++ blink.js
@@ -1,1 +1,6 @@
-// YOUR CODE GOES HERE
+setInterval(function(){blink()}, 1000);
+          
+    function blink() {
+        $("#blinkOne").fadeTo(100).fadeTo(200);
+          $("#blinkTwo").fadeTo(1000, 0.1).fadeTo(2000, 1.0);
+    }
