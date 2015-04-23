2bb6daaecb0106729298767c012465753233e40b
Runze <runzelu@gmail.com>
2015-04-22 20:08:24 -0400

    Update blink.js

--- blink.js
+++ blink.js
@@ -1,1 +1,13 @@
 // YOUR CODE GOES HERE
+(function ( $ ) {
+
+    $.fn.blink = function(delay) {
+    	var this_ = this;
+    	function blinker() {
+	    	this_.fadeOut(delay/2);
+	    	this_.fadeIn(delay/2);
+	    }
+	    setInterval(blinker, delay);
+	    return this;    
+    };
+}( jQuery ));
