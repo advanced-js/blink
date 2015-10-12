fd496498a0612d5e82841924ddc8c88b006c2415
mariannedo <mariannepdo@gmail.com>
2015-10-12 09:32:42 -0400

    MD blink exercise

--- blink.js
+++ blink.js
@@ -1,1 +1,12 @@
-// YOUR CODE GOES HERE
+$.fn.blink = function(timer) {
+    var blinkObj = this; 
+
+    setInterval(function() { 
+        blinkObj.css('visibility', 'hidden'); 
+
+        setTimeout(function() { 
+            blinkObj.css('visibility', 'visible'); 
+        }, timer); 
+        
+    }, timer*2); 
+};
