49eaae8e9681c96f33e2cc54ae0db4e6b2050340
mnsinger <mnsinger@gmail.com>
2015-10-12 09:36:39 -0400

    Modified jquery to accept argument
    Works for variable blink speeds now.

--- blink.js
+++ blink.js
@@ -1,6 +1,6 @@
 // YOUR CODE GOES HERE
 
-$.fn.blink = function(){
+$.fn.blink = function( msecs ){
   var that = $(this);
   setInterval(
     function()
@@ -8,5 +8,5 @@ $.fn.blink = function(){
         if (that.css("color") == 'rgb(0, 0, 0)') { that.css("color", "#ffffff"); }
         else { that.css("color", "#000000"); }
       }
-  , 1000 );
+  , msecs );
 }
