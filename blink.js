4ff6f3b1b3984219ee292bd53228428f2d255240
mnsinger <mnsinger@gmail.com>
2015-10-11 21:06:00 -0400

    Not working yet...

--- blink.js
+++ blink.js
@@ -1,1 +1,16 @@
 // YOUR CODE GOES HERE
+
+$.fn.blink = function(milliseconds) {
+  alert(this.css("color"));
+  alert(this.css("color") == 'rgb(0, 0, 0)');
+  //alert(this.css("color"));
+  //this.css( "color", "orange" );
+  window.setInterval(
+      function() { 
+        if ( this.css("color") == 'rgb(0, 0, 0)' ) { this.css( "color", "orange" ); }
+        //else { $(this).css( "color", "black"); }
+        return this;
+      }, 1000);
+
+};
+ 
