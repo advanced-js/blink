c5cd44c18b02063c5e767cf562e844456ff35d30
Kristian <jankristianlindberg@gmail.com>
2015-10-14 13:38:32 -0400

    This version meets requirements for both V1 and V2

--- blink.js
+++ blink.js
@@ -1,1 +1,13 @@
-// YOUR CODE GOES HERE
+(function ($) {
+	$.fn.blink = function(y){
+		var x = this;
+		setInterval(function() {
+      if (x.css('visibility') == 'visible') {
+    		x.css('visibility', 'hidden');
+    	 }
+    	else {
+     	 x.css('visibility', 'visible');
+    	}
+  	}, y);
+	};
+}( jQuery ));
