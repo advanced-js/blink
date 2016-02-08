785f1250363798185f78d5c44e0d5c51f5df75c9
bluemittens505 <blaird2e@verizon.net>
2016-02-07 22:24:40 -0500

    Initial version of V1

--- blink.js
+++ blink.js
@@ -1,1 +1,14 @@
-// YOUR CODE GOES HERE
+// Write a jQuery plugin that makes an element act like a  <blink>  tag.
+// It should work for any arbitrary speed.
+
+// V1
+// Make plugin work for selectors that correspond to a single element.
+
+$.fn.blink = function() {
+	setInterval(blinkit,1000,this);
+};
+
+function blinkit(obj) {
+	var value = obj.css('visibility') === 'visible' ? 'hidden' : 'visible';
+	obj.css('visibility',value);
+}
