46444e1444d44c8f6f59831363ebab502973d079
bluemittens505 <blaird2e@verizon.net>
2016-02-08 21:24:53 -0500

    V2
    Make plugin work for selectors that correspond to multiple elements.

--- blink.js
+++ blink.js
@@ -1,14 +1,15 @@
 // Write a jQuery plugin that makes an element act like a  <blink>  tag.
 // It should work for any arbitrary speed.
 
-// V1
-// Make plugin work for selectors that correspond to a single element.
+// V2
+// Make plugin work for selectors that correspond to multiple elements.
 
 (function ( $ ) {
 
 	$.fn.blink = function(duration) {
-		setInterval(blinkit,duration,this);
-		return this;
+	    return this.each(function() {
+	    	setInterval(blinkit,duration,$(this));
+	    });
 	};
 
 }( jQuery ));
@@ -17,3 +18,21 @@ function blinkit(obj) {
 	var value = obj.css('visibility') === 'visible' ? 'hidden' : 'visible';
 	obj.css('visibility',value);
 }
+
+
+// V1
+// Make plugin work for selectors that correspond to a single element.
+
+// (function ( $ ) {
+
+// 	$.fn.blink = function(duration) {
+// 		setInterval(blinkit,duration,this);
+// 		return this;
+// 	};
+
+// }( jQuery ));
+
+// function blinkit(obj) {
+// 	var value = obj.css('visibility') === 'visible' ? 'hidden' : 'visible';
+// 	obj.css('visibility',value);
+// }
