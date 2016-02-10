4356f00ffe4c669ffcfed5cec68978b33195ff3c
clenclen <dannyphu@gmail.com>
2016-02-10 15:03:07 -0500

    question
    would like to understand how to accomplish same effects change displays
    or using hide/show

--- blink.js
+++ blink.js
@@ -1,9 +1,18 @@
-// YOUR CODE GOES HERE
+// // YOUR CODE GOES HERE
 $.fn.blink = function(seconds) {
 	var $this = this
 	setInterval(function(){
 		$this.toggle(); 
 	},seconds);
 };
+
+// i don't understand how to do this using hide/show methods. :/ 
+
+// $.fn.blink = function(seconds) {
+// 	var $this = $(this)
+// 	setInterval(function(){
+// 		$this.style.display == "none" ? "block" : "none";
+// 	},seconds);
+// };
 
  
